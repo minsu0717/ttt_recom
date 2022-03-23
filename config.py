dad22d27f3962900b9472a4b9ffbdfd01b650872
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2022-03-23 15:07:49 +0900

    ...

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config :
     JWT_SECRET_KEY = 'yh@1234'
     S3_BUCKET = "alstn-project"
-    S3_KEY = "AKIAY2KXTZ5XALFXA2MU"
+    S3_KEY = 'AKIAV3VCDNSRTWEDELOS'
     S3_SECRET = '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'
     S3_LOCATION = 'https://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
